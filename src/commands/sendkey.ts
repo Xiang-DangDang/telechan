7f7d035f4b97df0730025fe4810ba676483e2207
Steven <chunmeng.jiang@live.com>
2021-06-25 18:02:31 +0800

    更新
    1. 将 消息向个人发送,修改为向指定会话发送
    2. 增加了 OWNER_ID 环境配置, 标识 API 所有者,防止 API 非法调用, 只有所有者可以获取 sendkey

--- src/commands/sendkey.ts
+++ src/commands/sendkey.ts
@@ -1,24 +1,34 @@
 import md5 from 'md5';
 const VERCEL_URL = process.env.VERCEL_URL;
 const TCKEY = process.env.TCKEY;
+const OWNER_ID = process.env.OWNER_ID;
 
 const replyToMessage = (ctx: any, messageId: string, string: string) =>
-  ctx.reply(string, {
-    reply_to_message_id: messageId,
-  });
+	ctx.reply(string, {
+		reply_to_message_id: messageId,
+	});
 
 const sendkey = () => (ctx: any) => {
+	const messageId = ctx.message.message_id;
 
-  const messageId = ctx.message.message_id;
-  const userName = ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
-  const sendkey = ctx.from.id +'T'+md5(TCKEY+ctx.from.id);
+	if (OWNER_ID != ctx.from.id) {
+		replyToMessage(ctx, messageId, `Sorry!\nYou are not the owner of the robot and are not allowed to use this API !!!`)
+	}
 
-  const site_url = String(VERCEL_URL).split("-")[0]+'.vercel.app';
+	const userName = ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
+	const chatId = ctx.message.chat.id;
+	const sendkey = chatId+ 'T' + md5(TCKEY+chatId);
 
-  replyToMessage(ctx, messageId, `${userName} , Your sendkey is 🔑 ${sendkey} \n 
-  🚀 Use follow url to send message : \n 
+	const site_url = String(VERCEL_URL).split("-")[0] + '.vercel.app';
+
+	ctx.telegram.sendMessage(ctx.from.id,`${userName} , Your sendkey is 🔑 ${sendkey} \n 
+	🚀 Use follow url to send message to the chat of ${chatId} : \n 
+	${site_url}/api/send?sendkey=<sendkey>&text=<text>`);
+
+	replyToMessage(ctx, messageId, `${userName} , Your sendkey is 🔑 ${sendkey} \n 
+  🚀 Use follow url to send message to the chat of ${chatId} : \n 
   ${site_url}/api/send?sendkey=<sendkey>&text=<text>`);
-  // replyToMessage(ctx, messageId, `Hello, ${userName} (user_id: ${ctx.from.id})! \n Your Message id is: ${messageId}`);
+	// replyToMessage(ctx, messageId, `Hello, ${userName} (user_id: ${ctx.from.id})! \n Your Message id is: ${messageId}`);
 };
 
-export { sendkey };
+export { sendkey, replyToMessage };
